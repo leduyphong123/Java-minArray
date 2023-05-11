d747fe29447f33eee389ce2b4e159487f93a9ea7
leduyphong123 <leduyphongck789@gmail.com>
2023-05-12 01:15:12 +0700

    finnish

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
-        int[] arr={2,3,1,4,5};
-        System.out.print("index "+arr[minValue(arr)]);
-    }
-    public static int minValue(int[] arr){
-        int index=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<arr[index]){
-                index=i;
+        Scanner input=new Scanner(System.in);
+        int number;
+
+        do{
+            System.out.println("input number array");
+             number=input.nextInt();
+        }while(number<1);
+        int[] arr=new int[number];
+        for(int i=0;i<number;i++){
+            System.out.println("Input arr index " + i);
+            arr[i]=input.nextInt();
+        }
+        int min=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(min>arr[i]){
+                min=arr[i];
             }
         }
-        return index;
+        System.out.println("min or array = " +min);
     }
 }
